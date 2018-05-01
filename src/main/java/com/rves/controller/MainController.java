baa70e596bb57deb3174b9e27f029df808df7f7a
Gesheft-Rves <Rves07@yandex.ru>
2018-05-01 15:54:24 +0300

    разбиение содержимого для web  по ролям

--- src/main/java/com/rves/controller/MainController.java
+++ src/main/java/com/rves/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.rves.controller;
 
+import com.rves.services.BookingService;
+import com.rves.services.RoomsService;
+import com.rves.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -10,12 +14,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RoomsService roomsService;
+    @Autowired
+    private BookingService bookingService;
+
     @RequestMapping({"/", "/home"})
-    public String index(Model model){
+    public String mainController(Model model){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String name = auth.getName(); //get logged in username
-        model.addAttribute("message", "Добро пожаловать в отель. Создай мне чуточку броней на сегодня, " + name);
+        String role = String.valueOf(userService.loadUserByUsername(name).getAuthorities());
+
+        switch (role) {
+            case ("ROLE_ADMIN"):
+                model.addAttribute("message", "Добро пожаловать в отель, " + name);
+                updateModelForAdmin(model);
+                return "adminIndex";
+            case "[ROLE_CLEANER]":
+                model.addAttribute("message", "Добро пожаловать в отель, " + name);
+                updateModelForCleaner(model);
+                return "cleanerIndex";
+            case "[ROLE_USER]":
+                updateModelForUser(model);
+                model.addAttribute("message", "Добро пожаловать в отель. Создай мне чуточку броней на сегодня, " + name);
+                return "userindex";
+        }
         return "index";
+    }
+
+    private void updateModelForAdmin(Model model){
+        model.addAttribute("rooms",roomsService.unclearedRooms());
+        model.addAttribute("bookings",bookingService.bookingEndTimeList());
+    }
+
+    private void updateModelForCleaner(Model model){
+        model.addAttribute("rooms",roomsService.unclearedRooms());
+    }
+
+    private void updateModelForUser(Model model){
+        model.addAttribute("bookings",bookingService.bookingEndTimeList());
     }
 
     @GetMapping("/403")
