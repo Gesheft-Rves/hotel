f0b2e54f78a85519ba40f8acd8470df651f957de
Gesheft-Rves <Rves07@yandex.ru>
2018-05-04 11:46:07 +0300

    testA

---
+++ src/main/java/com/rves/bootstrap/Test.java
@@ -0,0 +1,12 @@
+package com.rves.bootstrap;
+
+public class Test {
+    String s;
+    int x;
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.s = "asd";
+        test.x = 5;
+    }
+}
